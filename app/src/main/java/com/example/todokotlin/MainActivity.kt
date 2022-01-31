878fbb4561574029ab9538464739260e6591c173
moMMo <mohammad_sadeghi96@yahoo.com>
2022-01-31 18:53:39 +0100

    last V.

--- app/src/main/java/com/example/todokotlin/MainActivity.kt
+++ app/src/main/java/com/example/todokotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.todokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var todoAdapter : TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        todoAdapter = TodoAdapter(mutableListOf())
+        rvToDoItems.adapter = todoAdapter
+        rvToDoItems.layoutManager = LinearLayoutManager(this)
+
+//        Add button clicked
+        btnAddToDo.setOnClickListener {
+            val todoTitle = etToDoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addToDO(todo)
+                etToDoTitle.text.clear()
+            }
+        }
+//        Delete button clicked
+        btnDeleteDoneToDOs.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
